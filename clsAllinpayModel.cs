7e648c9e6d026797109fa98b04a7d5e596a39ba3
Lynn Naing <lynnnaingzaw@gmail.com>
2023-03-16 14:26:52 +0800

    create clsAllinPayModel for 3.1.1
    clsAllinPayModel for 3.1.1 Collection Request & Return Bank Card

---
+++ clsAllinpayModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.Expando;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using System.Xml.Linq;
+
+namespace Allinpay
+{
+    public class clsAllinpayModel
+    {
+        public string BUS_INFO { get; set; } // Optional; 
+        public string DCC_FLAG { get; set; } // Optional; 
+        public string BUS_INFO { get; set; } // Optional; 
+        public string DCC_FLAG { get; set; } // Optional;
+        public string BUS_INFO { get; set; } // Optional;
+    }
+
+    #region 3.1.1 Collection – Bank card 
+    public class CollectionRequestBankCard
+    {
+        public int OPER_NO { get; set; } // Optional; Operator number
+        public int BUSINESS_ID { get; set; } // Required; Business type
+        public int AMOUNT { get; set; } // Required; Amount (12 digit-number, cent as unit,completes it by adding 0 on left if the digit numbers are insufficient)
+        public string MEMO { get; set; } // Optional; Meomo field
+        public int TRANS_TRACE_NO { get; set; } // Required; Transaction unique identifier
+        public int TRANS_ORDER_NO { get; set; } // Required; Commodity order number
+        public string CURRENCY { get; set; } // Required; Currency (refer to Appendix-Currency)
+        public string DCC_FLAG { get; set; } // Optional; Default EDC transaction （DCC/EDC）
+        public string BUS_INFO { get; set; } // Optional; Expand field
+    }
+
+    public class CollectionReturnBankCard
+    {
+        public int BUSINESS_ID { get; set; } // Required; Business type
+        public int AMOUNT { get; set; } // Required; Amount (12 digit-number, cent as unit,completes it by adding 0 on left if the digit numbers are insufficient)
+        public string CURRENCY { get; set; } // Required; Currency (refer to Appendix-Currency)
+        public string PAYCURRENCY { get; set; } // Optional; Payment currency
+        public double EXCHANGE_RATE { get; set; } // Optional; Exchange rate
+        
+        public double ACTUALLY_AMOUNT { get; set; } // Required; The amount actually paid
+        public string TRACE_NO { get; set; } // Required; Swift number
+        public DateTime EXP_DATE { get; set; } // Required; Valid Date
+        public string BATCH_NO { get; set; } // Required; Batch number
+        public string MERCH_ID { get; set; } // Required; Business number
+
+        public string MERCH_NAME { get; set; } // Required; Business name
+        public string TER_ID { get; set; } // Required; Terminal number
+        public string REF_NO { get; set; } // Required; System reference number
+        public string AUTH_NO { get; set; } // Required; Authorization code
+        public string REJCODE { get; set; } // Required; Return code
+
+        public string CARD_ORGN { get; set; } // Optional; Card organization
+        public string CARDNO { get; set; } // Required; Card number
+        public DateTime DATE { get; set; } // Required; Transaction date
+        public DateTime TIME { get; set; } // Required; Transaction time
+        public string REJCODE_CN { get; set; } // Required; Return code explanation
+
+        public string MEMO { get; set; } // Optional; Memo field
+        public string TRANS_TRACE_NO { get; set; } // Optional; Transaction unique identifier
+        public string BUS_INFO { get; set; } // Optional; Expand field
+        public string TRANS_TICKET_NO { get; set; } // Optional; Transaction number
+        public string OPER_NO { get; set; } // Optional; Operator number 
+    }
+    #endregion End of 3.1.1 Collection – Bank card 
+
+
+}
